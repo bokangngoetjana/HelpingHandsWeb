fcb84ffb3b31a18314b5917a625d143d96539305
Bokang Ngoetjana <s220689725@mandela.ac.za>
2023-09-12 22:16:51 +0200

    Added Admin role

---
+++ Data/AdminRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public static async Task SeedRolesAsync(IServiceProvider service)
+{
+    var userManager = service.GetService<UserManager<ApplicationUser>>();
+    var roleManager = service.GetService<RoleManager<IdentityRole<int>>>();
+
+    for
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
 ﻿using HelpingHands.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HelpingHands.Data
-{
+namespace HelpingHands.Data { 
+//{ 
+//    public class ApplicationUser : IdentityUser
+//    {
+//        public string? FirstName { get; set; }
+//        public string? LastName { get; set; }
+//        public string? Gender { get; set; }
+//        public DateOnly DateOfBirth { get; set; }
+//        public string? EmergencyPerson { get; set; }
+//        public string? EmergencyContactNo { get; set; }
+//    }
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
